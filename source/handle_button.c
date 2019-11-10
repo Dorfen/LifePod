8dd68849a09e635f15258f40dc37e470f606aee9
Zacharie Corniere <zacharie.corniere@epitech.eu>
2019-11-10 23:18:44 +0100

    feat(button): capture keystroke into action

--- source/handle_button.c
+++ source/handle_button.c
@@ -7,9 +7,41 @@
 
 #include "lifepod.h"
 
+bool is_good_input(unsigned int nb_buttons, char input)
+{
+    if (nb_buttons > 4)
+        return false;
+    switch (input) {
+        case OPT_ONE:
+        case OPT_TWO:
+        case OPT_THREE:
+        case OPT_FOUR:
+        case OPT_QUIT:
+            return true;
+        default:
+            return false;
+    }
+}
+
 int button_related(ship_t *ship, event_t *event)
 {
-    if (ship == NULL || event == NULL)
-        return 84;
-    return 0;
+    char c = 0;
+
+    if (ship == NULL || event == NULL || my_tablen((char const *const *)event->button) == 0)
+        return -1;
+    while (is_good_input(my_tablen((char const *const *)event->button), c) == false)
+        c = getch();
+    switch (c) {
+        case OPT_ONE:
+            return 1;
+        case OPT_TWO:
+            return 2;
+        case OPT_THREE:
+            return 3;
+        case OPT_FOUR:
+            return 4;
+        default:
+            return -1;
+    }
+    return -1;
 }
--- tests/handle_button_test.c
+++ tests/handle_button_test.c
@@ -8,16 +8,21 @@
 #include <criterion/criterion.h>
 #include "lifepod.h"
 
-Test(handle_button, ship_is_null)
+Test(button_related, ship_is_null)
 {
     event_t *val = alloc_event();
-    cr_assert_eq(button_related(NULL, val), 84);
+    cr_assert_eq(button_related(NULL, val), -1);
     free(val);
 }
 
-Test(handle_button, event_is_null)
+Test(button_related, event_is_null)
 {
     ship_t *val = alloc_ship();
-    cr_assert_eq(button_related(val, NULL), 84);
+    cr_assert_eq(button_related(val, NULL), -1);
     free(val);
+}
+
+Test(handle_button, tab_len_too_large)
+{
+    cr_assert_eq(is_good_input(5, '\0'), false);
 }
